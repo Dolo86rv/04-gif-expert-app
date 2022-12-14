6f18be122507dc1770bf4d652a8ea55bedab2d99
Dolorv86 <dolorv86@gmail.com>
2022-12-14 16:35:40 +0100

    agregando cambios

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -4,6 +4,7 @@ export const GifItem = ({title, id, url}) => {
     
     return (
     <div className="card">
+        <p>ID. {id}</p>
         <img src={url} alt={title} />
         <p>{ title }</p>
     </div>
